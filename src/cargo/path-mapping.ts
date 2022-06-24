f70164891b6acb64faf44b317221b23f9c5595e2
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-06-24 18:15:45 +0000

    - Refactor path mapping - Add unit test

--- src/cargo/path-mapping.ts
+++ src/cargo/path-mapping.ts
@@ -6,14 +6,21 @@ interface PathMapping {
 
 const pathMappings = new Map<string, PathMapping>([
   ["/index", { path: "" }],
-  ["/_404", { path: "*" }],
+  ["/_404", { path: "/*" }],
 ]);
 
+/*
+ *
+ */
 export function mappedPath(path: string): string {
   const parsedPath = parse(path);
   const mappedPath = pathMappings.get(`/${parsedPath.name}`);
   if (mappedPath) {
-    return `${parsedPath.dir}${mappedPath.path}`;
+    return `${parsedPath.dir}${
+      parsedPath.dir === "/"
+        ? mappedPath.path.replace(/\//, "")
+        : mappedPath.path
+    }`;
   }
   return path;
 }
